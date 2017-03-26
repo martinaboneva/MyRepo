bb66060c967b47b5acb25a4ff3985e4f415d8a71
martina.boneva <martina.k.boneva@gmail.com>
2017-03-26 20:51:18 +0100

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write one-word string as an input: ");
+            string input = Console.ReadLine();
+
+            bool isPalindrome = IsPalindrome(input);
+            Console.WriteLine($"Is the string a palindrome ? {isPalindrome}");
+
+        }
+        public static bool IsPalindrome(string input)
+        {
+            bool isPalindrome = false;
+            var reversed = input.Reverse().ToArray();
+            if (input == new string(reversed))
+                isPalindrome = true;
+
+            return isPalindrome;
+        }
+    }
+}
---
+++ Palindrome2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome2
+{
+    class Program
+    {
+            static void Main(string[] args)
+        {
+                Console.WriteLine("Write one-word string as an input: ");
+                string input = Console.ReadLine();
+
+                bool isPalindrome = IsPalindrome(input);
+                Console.WriteLine($"Is the string a palindrome ? {isPalindrome}");
+
+            }
+        public static bool IsPalindrome(string input)
+        {
+            bool isPalindrome = true;
+            for (int i = 0; i < input.Length/2; i++)
+            {
+                if (input[i] != input[input.Length - i - 1])
+                {
+                    isPalindrome = false;
+                }
+            }
+            return isPalindrome;
+        }
+    }
+}
